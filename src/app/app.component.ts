17701d4a36f3f401097f9f694daf1b60c82d9dca
Robbie Rankin <robbierankin15@gmail.com>
2025-09-13 03:16:04 -0400

    updated toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     RouterLink,
+    RouterLinkActive,
     MatToolbarModule,
     MatButtonModule,
-    RouterLinkActive,
+    MatIconModule,
+    MatMenuModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'nhl-chem';
+
+
 }
